c4b116bff074fd324438c6a3fdb771b15dcb447f
Charminz <indrek.toomsalu@gmail.com>
2018-11-25 22:15:53 +0200

    Build files

---
+++ dist/precache-manifest.bd793e3e32ed6db834b3f6f9a8dc1ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2c617b1098dc4c18a09b",
+    "url": "/js/chunk-vendors.f852af13.js"
+  },
+  {
+    "revision": "a9504e814d98d2ffb8e6",
+    "url": "/js/app.88e490bf.js"
+  },
+  {
+    "revision": "79e51620456167ab512d6705a7c44970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c617b1098dc4c18a09b",
+    "url": "/css/chunk-vendors.d3a84e87.css"
+  },
+  {
+    "revision": "a9504e814d98d2ffb8e6",
+    "url": "/css/app.9801655d.css"
+  }
+];
